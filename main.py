2f52bcf62bbacfb2eb62b2828a25496bc0e4802f
Krishnag09 <krishnag0902@gmail.com>
2022-08-15 00:20:48 -0400

    working post

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 from flask_restful import Resource, Api, reqparse
 import pandas as pd
+import random
 
 app = Flask(__name__)
 api = Api(app)
@@ -37,8 +38,36 @@ class Posts(Resource):
             action ={}
             action[int(action_id)] = actions[int(action_id)]
             return jsonify(action)
-    
+class NewPosts(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+
+        self.reqparse.add_argument('title', type=str, help = "action desc missing")
+        self.reqparse.add_argument('description', type=str, help="action image is missing")
+        self.reqparse.add_argument('impact', type=str, help="action impact is missing")
+        self.reqparse.add_argument('image', type=str, help= "action title is missing")
+        self.reqparse.add_argument('category', type=str, help= "forgot the category?")
+
+    def post(self):
+        args= self.reqparse.parse_args()
+
+        action_info = { 
+            "action_title": args['title'], 
+            "action_description": args['description'], 
+            "action_impact": args['impact'],
+            "action_image": args['image'],
+            "category": args['category']
+            }
+        id = random.randint(10000,100000)
+        actions[id] = action_info
+        return id
+
+api.add_resource(NewPosts, '/api/newpost')
 api.add_resource(Posts,'/api/posts', '/api/posts/<action_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+# GET ALL POSTS http://127.0.0.1:5000/api/posts
+# GET POST BY ID http://127.0.0.1:5000/api/posts/action_id
+# GET MULTIPLE POSTS http://127.0.0.1:5000/api/posts/action_id1,action_id12
